4ed27714000ce9e0a5eef335abfd217ce127ff69
kasonyang <i@kasonyang.com>
2014-07-05 00:07:07 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,22 @@
+简介
+===========
+使 Hiano App 支持 Hitar 的类库
+
+
+安装
+===========
+通过composer安装
+
+
+composer.json
+
+    {
+      "require" : {
+        "kasonyang/hitar4hiano" : "*"
+      }
+    }
+
+License
+=========
+MIT
+
---
+++ src/Hitar4Hiano.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * 
+ * @author kasonyang <i@kasonyang.com>
+ */
+
+namespace Hitar4Hiano;
+
+class Hitar4Hiano{
+    static function init(){
+        $dbs = \Hiano\App\App::getConfig('hitar:database');
+        $first = NULL;
+        foreach($dbs as $dk => $dv){
+            if($first === NULL){
+                $first = $dk;
+            }
+            \Hitar\DatabaseManager::addDatabase($dk, $dv);
+        }
+        if($first){
+            \Hitar\DatabaseManager::selectDatabase($first);
+        }
+    }
+}
+
+Hitar4Hiano::init();
